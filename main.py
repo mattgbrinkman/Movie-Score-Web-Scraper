75d34f8920ef160d586bb375a9219e7e1559e197
mattgbrinkman <matthewgbrinkman@gmail.com>
2024-08-07 19:22:15 -0700

    clean up files v2

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ from asyncio import run
 from pipeline_utils import Extract, Transform, Load
 
 if __name__ == "__main__":
-    # Extract, transform and load data
     try:
         ex = Extract()
-        tf = Transform()
-        ld = Load()
         movie_dict = Extract.movie_titles(ex)
         result = run(ex.get_scores_tasks(movie_dict))
-        df = Transform.transform_data(ld, movie_dict)
+        
+        tf = Transform()
+        df = Transform.transform_data(tf, movie_dict)
+
+        ld = Load()
         Load.insert_to_db(ld, df)
 
     except Exception as e:
